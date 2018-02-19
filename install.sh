e81bcefd4fdaf113253fe52f9d500cd06042c85d
bethtelford <mrsbethtelford@gmail.com>
2018-02-19 12:47:10 -0700

    added open command to setup file

--- install.sh
+++ install.sh
@@ -16,3 +16,13 @@ echo "✔ apt dev tools installed"
 sudo npm install -g create-react-app nodemon live-server
 echo "✔ npm -g dev tools installed"
 
+echo "open() {" >> ~/.bashrc
+echo "last=\${1: -1}" >> ~/.bashrc
+echo "if [ \$last = / ]" >> ~/.bashrc
+echo "then" >> ~/.bashrc
+echo "file=\${1::-1}" >> ~/.bashrc
+echo "else" >> ~/.bashrc
+echo "file=\$1" >> ~/.bashrc
+echo "fi" >> ~/.bashrc
+echo "cmd.exe /C start "\${file}"}" >> ~/.bashrc
+echo "✔ open command configured"
